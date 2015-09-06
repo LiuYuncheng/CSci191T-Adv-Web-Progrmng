4543dc864b835534d7e5515a77a81c857666695f
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-09-05 23:57:45 -0700

    Create pointers.go

---
+++ Presentation-11---Exercises/pointers/pointers.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main(){
+var x int =2
+var y *int =&x
+
+fmt.Println("x's value is ", x)
+fmt.Println("x's address is ", &x)
+fmt.Println("y's value is ", y)
+fmt.Println("y points to ", *y)
+}
