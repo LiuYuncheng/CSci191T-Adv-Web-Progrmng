3c35fb816291ca7acec43a835d5a14577cce4583
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-09-06 23:32:28 -0700

    Update escapeSequences.go

--- Presentation-14---Exercises/escapeSequences/escapeSequences.go
+++ Presentation-14---Exercises/escapeSequences/escapeSequences.go
@@ -5,11 +5,3 @@ func main(){
 fmt.Print ("This is a tab\ttab.")
 fmt.Print ("\nThis is a newline.")
 }
-
-package main
-import "fmt"
-
-func main () {
-	fmt.Print ("This is a tab\ttab.")
-	fmt.Print ("\nThis is a newline.")
-}
