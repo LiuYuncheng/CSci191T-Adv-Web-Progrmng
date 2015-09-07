231c861841da76e8a606bcfa4dd712d422a3f4e4
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-09-06 23:25:17 -0700

    Create concat.go

---
+++ Presentation-14---Exercises/concat/concat.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+first := "yuncheng"
+last := "liu"
+englishName := "tony"
+
+fmt.Println("My name is " + first + " " + last)
+fmt.Println("My English name is " + englishName + " " + last)
+}
