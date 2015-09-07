e49da7c9423298a94e8005f7202492821ff1fdce
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-09-07 12:26:04 -0700

    Update bitwise.go

--- Presentation-11---Exercises/bitwise/bitwise.go
+++ Presentation-11---Exercises/bitwise/bitwise.go
@@ -6,11 +6,11 @@ var x uint = 10
 var y uint = 10
 fmt.Println("x is ", x)
 fmt.Println("y is ", y)
-x = x << `
+x = x << 1
 y = y >> 1
 fmt.Println("x now ", x)
 fmt.Println("y now ", y)
 }
 // 10: 000 1010
-//10<<1(shift one bit left): 0001 0100 (2^4 + 2^2 = 20)
+// 10<<1(shift one bit left): 0001 0100 (2^4 + 2^2 = 20)
 // 10>>1(shift one bit right: 0000 0101) (2^2 + 2^0 = 5)
