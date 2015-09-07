c182c6cabfc556a1583637255742b8d303cfbff7
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-09-07 12:25:16 -0700

    Update bitwise.go

--- Presentation-11---Exercises/bitwise/bitwise.go
+++ Presentation-11---Exercises/bitwise/bitwise.go
@@ -2,12 +2,15 @@ package main
 import "fmt"
 
 func main(){
-var x uint = 1
-var y uint = 2
+var x uint = 10
+var y uint = 10
 fmt.Println("x is ", x)
 fmt.Println("y is ", y)
-x = 1 << x
+x = x << `
 y = y >> 1
 fmt.Println("x now ", x)
 fmt.Println("y now ", y)
 }
+// 10: 000 1010
+//10<<1(shift one bit left): 0001 0100 (2^4 + 2^2 = 20)
+// 10>>1(shift one bit right: 0000 0101) (2^2 + 2^0 = 5)
