7516e1ec23c8beaa1559deb30266aafcfb773cf3
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-09-07 20:00:46 -0700

    Update loops.go

--- Presentation-12---Exercises/loops/loops.go
+++ Presentation-12---Exercises/loops/loops.go
@@ -3,7 +3,7 @@ import "fmt"
 
 func main(){
 for i := 0; i<10;i++{
-if i % 2 ==0{
+if i % 3 ==0{
 fmt.Print (i, " ")
 }
 }
@@ -12,7 +12,7 @@ fmt.Println()
 
 i :=0
 for i<10{
-if i % 2 == 0{
+if i % 3 == 0{
 fmt.Print (i, " ")
 }
 
@@ -27,7 +27,7 @@ if i>=10{
 break
 }
 
-if i % 2 ==0{
+if i % 3 ==0{
 fmt.Print (i, " ")
 }
 
