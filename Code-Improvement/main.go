4c977038d6b78ae9bd586d759941ce70d4860004
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-12-19 02:33:59 -0700

    Create main.go

---
+++ Code-Improvement/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+  )
+
+var tpl *template.Template
+
+func init() {
+	r := httprouter.New()
+	http.Handle("/", r)
+	r.GET("/", Home)
+	r.GET("/form/login", Login)
+	r.GET("/form/signup", Signup)
+	r.POST("/api/checkusername", checkUserName)
+	r.POST("/api/createuser", createUser)
+	r.POST("/api/login", loginProcess)
+	r.GET("/api/logout", logout)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
+	tpl = template.Must(template.ParseGlob("templates/html/*.html"))
+  }
+
+func Home(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	
+	serveTemplate(res, req, "home.html")
+  }
+
+func Login(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	
+	serveTemplate(res, req, "login.html")
+  }
+
+func Signup(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	
+	serveTemplate(res, req, "signup.html")
+  }
+
