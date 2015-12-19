fba8b0669bf43c7fd571c535bde423bb025b080a
LiuYuncheng <lyc881223@mail.fresnostate.edu>
2015-12-19 02:35:08 -0700

    Create model.go

---
+++ Code-Improvement/model.go
@@ -0,0 +1,15 @@
+package main
+
+type User struct {
+	
+	Email    string
+	UserName string `datastore:"-"`
+	Password string `json:"-"`
+  }
+
+type SessionData struct {
+	
+	User
+	LoggedIn  bool
+	LoginFail bool
+  }
